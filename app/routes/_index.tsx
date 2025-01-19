3a29b135a28c1eb822ce1186115948ba28190547
ParamBirje <parambirje@gmail.com>
2025-01-19 20:12:26 +0530

    clock is now hidden

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,6 @@
 import type { MetaFunction } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import BrowserSearch from "~/components/browser-search";
-import Clock from "~/components/clock";
 import FreeTodaySection from "~/components/free-today";
 import { GiveawaysSortedByValue } from "~/services/api/giveaways";
 import { Giveaway } from "~/types";
@@ -27,7 +26,7 @@ export default function Index() {
 
   return (
     <main className="max-w-5xl mx-auto px-6 py-16 flex flex-col gap-16 min-h-screen">
-      <Clock />
+      {/* <Clock /> */}
       <BrowserSearch />
       <FreeTodaySection popular={popular} />
     </main>
